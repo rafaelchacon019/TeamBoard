cc741495215ee7701fb324a8638a6fb962f21d6e
rafaelchacon019 <rafael.chacon029@gmail.com>
2021-12-09 14:37:09 -0500

    crud completa de task y se añaden estilos

--- frontend/src/app/board/list-task/list-task.component.ts
+++ frontend/src/app/board/list-task/list-task.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { BoardService } from '../../services/board.service';
+import {
+  MatSnackBar,
+  MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition,
+} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-list-task',
@@ -6,10 +12,79 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-task.component.css']
 })
 export class ListTaskComponent implements OnInit {
+  taskData: any;
+  message: string = '';
+  horizontalPosition: MatSnackBarHorizontalPosition = 'end';
+  verticalPosition: MatSnackBarVerticalPosition = 'top';
+  durationInSeconds: number = 2;
 
-  constructor() { }
+  constructor(
+    private _boardService: BoardService,
+    private _snackBar: MatSnackBar
+  ) {
+    this.taskData = {};
+  }
 
   ngOnInit(): void {
+    this._boardService.listTask().subscribe({
+      next: (v) => {
+        this.taskData = v.taskList;
+      },
+      error: (e) => {
+        this.message = e.error.message;
+        this.openSnackBarError();
+      }
+    });
+  }
+
+  updateTask(task:any, status: string){
+    let tempStatus = task.taskStatus;
+    task.taskStatus = status;
+    this._boardService.updateTask(task).subscribe({
+      next: (v) => {
+        task.taskStatus = status;
+      },
+      error: (e) => {
+        task.taskStatus = tempStatus;
+        this.message = e.error.message;
+        this.openSnackBarError();
+      }
+    });
+  }
+
+  deleteTask(task: any){
+    this._boardService.deleteTask(task).subscribe({
+      next: (v) => {
+        let index = this.taskData.indexOf(task);
+        if (index > -1) {
+          this.taskData.splice(index, 1);
+          this.message = v.message;
+          this.openSnackBarSuccesfull();
+        }
+      },
+      error: (e) => {
+        this.message = e.error.message;
+        this.openSnackBarError();
+      }
+    });
+  }
+
+  openSnackBarSuccesfull() {
+    this._snackBar.open(this.message, 'X', {
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+      duration: this.durationInSeconds * 1000,
+      panelClass: ['style-snackbarTrue'],
+    });
+  }
+
+  openSnackBarError() {
+    this._snackBar.open(this.message, 'X', {
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+      duration: this.durationInSeconds * 1000,
+      panelClass: ['style-snackbarFalse'],
+    });
   }
 
 }
